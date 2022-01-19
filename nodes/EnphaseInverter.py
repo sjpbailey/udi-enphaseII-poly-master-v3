99ca67382c0ac329c9616a87033e4614aed01866
sjpbailey <sjpbailey1961@gmail.com>
2022-01-18 18:20:21 -0800

    Update EnphaseInverter.py

--- nodes/EnphaseInverter.py
+++ nodes/EnphaseInverter.py
@@ -67,8 +67,7 @@ class InverterNode(udi_interface.Node):
             self.system_id
         params = (('key', self.key), ('user_id', self.user_id))
         try:
-            r = json.loads(requests.get(
-                URL_SITE, params=params).decode("utf-8"))
+            r = requests.get(URL_SITE, params=params).decode("utf-8")
             #r = requests.get(URL_SITE, params=params)
             LOGGER.info(r.text)
             response = json.loads(r.text)
