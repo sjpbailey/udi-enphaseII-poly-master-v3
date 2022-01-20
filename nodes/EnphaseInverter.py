af7e110bfb9b173bf1e2e1e1986346d8bd27e04c
sjpbailey <sjpbailey1961@gmail.com>
2022-01-19 16:28:48 -0800

    Update EnphaseInverter.py

--- nodes/EnphaseInverter.py
+++ nodes/EnphaseInverter.py
@@ -24,7 +24,7 @@ LOGGER = udi_interface.LOGGER
 
 
 class InverterNode(udi_interface.Node):
-    def __init__(self, polyglot, primary, address, name, system_id, key, user_id, inv_idx, ):
+    def __init__(self, polyglot, primary, address, name, system_id, key, user_id, inv_idx):
         super(InverterNode, self).__init__(polyglot, primary, address, name)
         self.poly = polyglot
         self.lpfx = '%s:%s' % (address, name)
@@ -71,6 +71,10 @@ class InverterNode(udi_interface.Node):
             r2 = requests.get(URL_SITE, params=params)
             # LOGGER.info(r2)
             Response2 = json.loads(r2.text)
+            if r2.status_code == 200:
+                self.setDriver('ST', 1)
+            else:
+                self.setDriver('ST', 0)
         except requests.exceptions.RequestException as e:
             LOGGER.error("Error: " + str(e))
         #### Sort Inverter Data ####
@@ -80,22 +84,33 @@ class InverterNode(udi_interface.Node):
         df['type'] = np.where(df['energy.value'], 'inverter', df['type'])
         inverters = df[df['type'] == 'inverter'].reset_index(drop=True)
         # inverter string
-        if self.system_id is not None:
-            device_list = [inverters]
-            for device in device_list:
-                for idx, row in device.iterrows():
-                    inv_id = row['id']
-                    name = 'Inverter' + '-%s' % (idx+1)
-                    inv_serial = row['serial_number']
-                    inv_status = row['status']
-                    inv_kWh = row['energy.value']
-                    inv_kW = row['power_produced']
-                    address = row['type'] + '_%s' % (idx+1)
-                    inv_idx = '%s' % (idx)
-                    LOGGER.info('\nID\n{inv_id}\nSerial\n{inv_serial}\nStatus\n{inv_status}\nkWh\n{inv_kWh}\nkW\n{inv_kW}\nIndex\n{inv_idx}\n'.format(
-                        inv_id=inv_id, inv_serial=inv_serial, inv_status=inv_status, inv_kWh=inv_kWh, inv_kW=inv_kW, inv_idx=inv_idx))
-                    LOGGER.info(inv_kW)
-                    self.setDriver('GV1', inv_kW)
+        device_list = [inverters]
+        for device in device_list:
+            for idx, row in device.iterrows():
+                inv_id = row['id']
+                name = 'Inverter' + '-%s' % (idx+1)
+                inv_serial = row['serial_number']
+                inv_status = row['status']
+                inv_kWh = row['energy.value']
+                inv_kW = row['power_produced']
+                #address = row['type'] + '_%s' % (idx+1)
+                #inv_idx = '%s' % (idx)
+                # LOGGER.info('\nID\n{inv_id}\nSerial\n{inv_serial}\nStatus\n{inv_status}\nkWh\n{inv_kWh}\nkW\n{inv_kW}\nIndex\n{inv_idx}\n'.format(
+                #    inv_id=inv_id, inv_serial=inv_serial, inv_status=inv_status, inv_kWh=inv_kWh, inv_kW=inv_kW, inv_idx=inv_idx))
+                LOGGER.info(inv_kW)
+                self.setDriver('GV1', inv_kW)
+                LOGGER.info(inv_kWh)
+                self.setDriver('GV2', inv_kWh)
+                LOGGER.info(inv_serial)
+                self.setDriver('GV3', inv_serial)
+                LOGGER.info(inv_id)
+                self.setDriver('GV5', inv_id)
+                LOGGER.info(inv_status)
+                normal1 = inv_status
+                if normal1 == 'normal':
+                    self.setDriver('GV4', 1)
+                else:
+                    self.setDriver('GV4', 0)
 
             """if (r.status_code == 200):
                 #LOGGER.info('Energy values are currently present')
