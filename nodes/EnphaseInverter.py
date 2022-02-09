2a726d186f832ae4d51be914571ed14e6811a2ae
sjpbailey <sjpbailey1961@gmail.com>
2022-02-09 12:22:33 -0800

    Update EnphaseInverter.py

--- nodes/EnphaseInverter.py
+++ nodes/EnphaseInverter.py
@@ -84,7 +84,7 @@ class InverterNode(udi_interface.Node):
                 #address = row['type'] + '_%s' % (idx+1)
                 inv_idx = '%s' % (idx)
                 LOGGER.info('\nNodes\n\nname\n{name}\nID\n{inv_id}\nSerial\n{inv_serial}\nStatus\n{inv_status}\nkWh\n{inv_kWh}\nkW\n{inv_kW}\nIndex\n{inv_idx}\n'.format(
-                    name=name, inv_id=inv_id, inv_serial=inv_serial/10000, inv_status=inv_status, inv_kWh=inv_kWh, inv_kW=inv_kW, inv_idx=inv_idx))  # , inv_kW=inv_kW ## \nkW\n{inv_kW}
+                    name=name, inv_id=inv_id, inv_serial=inv_serial, inv_status=inv_status, inv_kWh=inv_kWh, inv_kW=inv_kW, inv_idx=inv_idx))  # , inv_kW=inv_kW ## \nkW\n{inv_kW}
                 LOGGER.info(inv_kW)
                 if inv_kWh > 1:
                     self.setDriver('GV1', inv_kW)
