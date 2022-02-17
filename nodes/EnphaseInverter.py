48339408639940dbe5a2d626be6aa25caff1b7fc
sjpbailey <sjpbailey1961@gmail.com>
2022-02-16 22:43:01 -0800

    Update EnphaseInverter.py

--- nodes/EnphaseInverter.py
+++ nodes/EnphaseInverter.py
@@ -115,15 +115,17 @@ class InverterNode(udi_interface.Node):
 
     # Do Not Poll unless you have power being produced  and 'GV1' != 0
     def poll(self, polltype):
-        if 'shortPoll' in polltype:
-            LOGGER.debug('shortPoll (node)')
-            sleeptime = random.uniform(60, 240)
-            sleep(sleeptime)
-            LOGGER.info("sleeping is over")
-            self.getpower(self)
-            # self.reportDrivers()
-        else:
-            LOGGER.debug('longPoll (node)')
+        inv_kW = inv_kW
+        if inv_kW != 0:
+            if 'shortPoll' in polltype:
+                LOGGER.debug('shortPoll (node)')
+                sleeptime = random.uniform(60, 240)
+                sleep(sleeptime)
+                LOGGER.info("sleeping is over")
+                self.getpower(self)
+                # self.reportDrivers()
+            else:
+                LOGGER.debug('longPoll (node)')
 
     def query(self, command):
         self.getpower(self)
