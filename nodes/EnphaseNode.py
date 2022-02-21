77c9b6d569e7cbb222f54a084a37439637dcf835
sjpbailey <sjpbailey1961@gmail.com>
2022-02-21 09:44:49 -0800

    Update EnphaseNode.py

--- nodes/EnphaseNode.py
+++ nodes/EnphaseNode.py
@@ -51,7 +51,7 @@ class SiteNode(udi_interface.Node):
             #kw1 = float(Response["current_power"]/1000*1000/220)
             #kw2 = float(kw1)*1000/220
             #LOGGER.info(str(kw2))
-            self.setDriver('GV11', 'GV1'*(1000)/220)
+            self.setDriver('GV11', float('GV1')*(1000)/220)
             LOGGER.info(Response["energy_today"])
             self.setDriver('GV2', float(Response["energy_today"]/1000))
             LOGGER.info(Response["energy_lifetime"])
